7482bc19ac07d051734a46aebe0e10db05fb7373
Seonu An <seonuan82@gmail.com>
2026-02-05 22:13:17 +0900

    Update iowa_gambling.py

--- iowa_gambling.py
+++ iowa_gambling.py
@@ -383,11 +383,11 @@ def display_results():
 
     st.markdown("---")
     
-    NEXT_EXPERIMENT_URL = "https://word-recall-101.streamlit.app/"
+    NEXT_EXPERIMENT_URL = "https://free-r-101.streamlit.app/"
     
     st.link_button(
         "▶ 다음 실험으로 이동",
-        "https://intertemporal-choice-task-5srsbs8qpesspk4szappzmk.streamlit.app/",
+        "https://free-r-101.streamlit.app/",
         use_container_width=True
     )
 
